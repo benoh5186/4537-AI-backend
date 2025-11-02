2d24ea8b4686bb17da936304784dc998c983b0f8
MarcusLages <marcusvlages@gmail.com>
2025-11-02 15:21:37 -0800

    Implemented basic route POST: /json/parse

--- routers/json/parse/endpoints.py
+++ routers/json/parse/endpoints.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
 from pathlib import Path
+from services.openai_service import OpenAIService
+from services.std_request import StdRequest
+from services.std_response import StdResponse
 
 _prefix = "/" + Path(__file__).parent.name
 router = APIRouter(prefix=_prefix)
 
-@router.get("/")
-def test():
-    # TODO: Implement routers
-    return {"message": "hellod"}
+@router.post("/")
+async def parse_text_to_json(req: StdRequest):
+    model = OpenAIService()
+    try:
+        response = await model.generate(req.text)
+        return StdResponse.success(response)
+    except ValueError as e:
+        response = "ValueError"
+        return StdResponse.error({"err_info" : e})
