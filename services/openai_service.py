f5722c996941fa0380708928a5c8e47c97390e8f
MarcusLages <marcusvlages@gmail.com>
2025-11-02 14:01:58 -0800

    Implemented AI Services for Open AI models

---
+++ services/openai_service.py
@@ -0,0 +1,70 @@
+import json
+import os
+import openai
+from typing import Any, Dict
+from .ai_service import AIService
+from messages.en import ErrorMsgs
+
+class OpenAIService(AIService):
+    """
+    Singleton class used to create an OpenAI Service client.
+    """
+    
+    DEFAULT_MODEL = "gpt-4o"
+    AI_RES_IDX = 0 # Index of the AI response in the list of messages
+    
+    _instance = None
+    _initialized = None
+    
+    def __new__(cls):
+        """
+        Constructor modified to allow Singleton functionality.
+        """
+        if cls._instance is None:
+            cls._instance = super(OpenAIService, cls).__new__(cls)
+        return cls._instance
+    
+    def __init__(self, system_prompt: str = None):
+        """
+        Initializes an OpenAI model service client.
+        :param system_prompt: String used to personalize the system prompt.
+        """
+        if self._initialized:
+            return
+        
+        self.model_name = os.getenv("OPENAI_MODEL", self.DEFAULT_MODEL)
+        if system_prompt:
+            super().__init__(self.model_name, system_prompt)
+        else:
+            super().__init__(self.model_name)
+        
+        self.__api_key = os.getenv("OPENAI_API_KEY")
+        if not self.__api_key:
+            raise ValueError(ErrorMsgs.MISSING_API_KEY)
+
+        self.model: openai.OpenAI = openai.AsyncOpenAI(
+            api_key=self.__api_key
+        )
+        
+        self._initialized = True
+
+    async def generate(self, prompt: str) -> Dict[str, Any]:
+        """
+        Generates a JSON response from the prompt.
+        :param prompt: String used as input to generate the JSON.
+        :throws ValueError: If the response from the AI model is not in a json format.
+        """
+        response = await self.model.responses.create(
+            model=self.model_name,
+            instructions=self.system_prompt,
+            input=prompt
+        )
+
+        content = response.output_text
+        print(response) # TODO: for debugging, delete this later
+        print(content) # TODO: for debugging, delete this later
+
+        try:
+            return json.loads(content)
+        except json.JSONDecodeError:
+            raise ValueError(ErrorMsgs.INVALID_JSON_RES)
